debb49e45c6cf4e839e1cddcd0b99bd33e1f9214
alex <alexanderrosen45@gmail.com>
2022-09-24 14:38:10 -0400

    two sum

---
+++ c++/two_sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i=0; i<size(nums)-1; i++) {
+            for (int j=i+1; j<size(nums); j++) {
+                if (nums[i]+nums[j] == target && i != j) {
+                    return {i, j};
+                }
+            }
+        }
+        return {-1};
+    }
+};
